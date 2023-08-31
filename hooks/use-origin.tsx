6387c781a23bf9c0acfa2e11b533057a4691b70d
Sharvil <karwasharvil@gmail.com>
2023-08-31 15:19:55 +0530

    hooks

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -1,11 +1,16 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const useOrigin = () => {
-    const [mounted, setMounted] = useState(false);
-    const origin = typeof window !== "undefined" ? window.location.origin : "";
-    useEffect(() => {
-        setMounted(true);
-    }
-    , []);
-    return mounted ? origin : "";
-}
+  const [mounted, setMounted] = useState(false);
+  const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return ''
+  }
+
+  return origin;
+};
